458aa1b435118653608df6a7ff85a1ae689c5097
Paul Clark <clark.paul123@gmail.com>
2018-10-19 14:45:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+=begin 
 
+def oxford_comma(array)
+  new_array = []
+    array.each do |item|
+      if array[-2] new_array.push("#{item} and")
+        elsif array[-1] new_array.push(" #item")
+      else new_array.push("#{item}, ")
+      end
+    end
+  new_array.join
 end
+
+=end
